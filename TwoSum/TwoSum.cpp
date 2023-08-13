3adf50f482e7b9c18017bb198eacb4fcadaf81d0
Drako31 <debernalg@unal.edu.co>
2023-08-13 18:46:20 -0500

    0

---
+++ TwoSum/TwoSum.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+
+    vector<int> twoSum(int nums[]&, int target) {
+
+        for (int i = 0; i < sizeof nums; ++i)
+        {
+            for (int j = 0; j < sizeof nums; ++j)
+            {
+                int sum = nums[i] + nums[j];
+                if (sum == target)
+                {
+                    
+                }
+            }
+        }
+
+    }
+};
+
+int main() {
+    Solution mySolution;
+
+}
