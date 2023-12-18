3218ad792777fe98d8c297893ecf3cf2930b321d
Drako31 <debernalg@unal.edu.co>
2023-12-17 19:08:47 -0500

    12-12-23.2

---
+++ DesignAfoodRatingSystem/2353.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class FoodRatings
+{
+
+public:
+    FoodRatings(vector<string> &foods, vector<string> &cuisines, vector<int> &ratings)
+    {
+    }
+    void changeRating(string food, int newRating)
+    {
+    }
+
+    string highestRated(string cuisine)
+    {
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
